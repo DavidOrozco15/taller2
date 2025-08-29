aacb1cd95553e0c128df7c171b52a1a9c3445807
DavidOrozco15 <205808288+DavidOrozco15@users.noreply.github.com>
2025-08-28 19:02:55 -0500

    3.0

--- cajero.py
+++ cajero.py
@@ -16,45 +16,60 @@ while isActive:
     print("0. Salir")
     
     opcion = int(input("\nSeleccione una opción: "))
-    
-    match opcion:
-        case 1:
-            os.system("clear")
-            print(f"\nSu saldo actual es: ${saldo}")
-            input("\nPresiona Enter para continuar...")
-            os.system("clear")
-        case 2:
-            os.system("clear")
-            deposito = int(input("\nIngrese la cantidad a depositar: "))
-            if deposito > 0:
-                saldo += deposito
-                print(f"\nDepósito exitoso. Su nuevo saldo es: ${saldo}")
+    try:
+        match opcion:
+            case 1:
+                os.system("clear")
+                print(f"\nSu saldo actual es: ${saldo}")
                 input("\nPresiona Enter para continuar...")
                 os.system("clear")
-            else:
-                print("\nCantidad inválida. Intente de nuevo.")
+            case 2:
+                try:
+                    os.system("clear")
+                    deposito = int(input("\nIngrese la cantidad a depositar: "))
+                    if deposito > 0:
+                        saldo += deposito
+                        print(f"\nDepósito exitoso. Su nuevo saldo es: ${saldo}")
+                        input("\nPresiona Enter para continuar...")
+                        os.system("clear")
+                    else:
+                        print("\nCantidad inválida. Intente de nuevo.")
+                        input("\nPresiona Enter para continuar...")
+                        os.system("clear")
+                except ValueError:
+                    print("\nCaracter Invalido, Intente de nuevo.")
+                    input("\nPresiona Enter para continuar...")
+                    os.system("clear")
+            case 3:
+                try:
+                    os.system("clear")
+                    retiro = int(input("\nIngrese la cantidad a retirar: "))
+                    
+                    if retiro > 0 and retiro <= saldo:
+                        saldo -= retiro
+                        print(f"\nRetiro exitoso. Su nuevo saldo es: ${saldo}")
+                        input("\nPresiona Enter para continuar...")
+                        os.system("clear")
+                    else:
+                        print("\nCantidad inválida o saldo insuficiente. Intente de nuevo.")
+                        input("\nPresiona Enter para continuar...")
+                        os.system("clear")
+                except ValueError:
+                    print("\nCaracter Invalido, Intente de nuevo.")
+                    input("\nPresiona Enter para continuar...")
+                    os.system("clear")
+            case 0:
+                os.system("clear")
+                print("\nGracias por usar el cajero automatico")
+                isActive = False
+            case _:
+                print("\nOpción inválida. Intente de nuevo.")
                 input("\nPresiona Enter para continuar...")
                 os.system("clear")
-        case 3:
-            os.system("clear")
-            retiro = int(input("\nIngrese la cantidad a retirar: "))
-            saldo -= retiro
-            if retiro > 0 and retiro <= saldo:
-                print(f"\nRetiro exitoso. Su nuevo saldo es: ${saldo}")
-                input("\nPresiona Enter para continuar...")
-                os.system("clear")
-            else:
-                print("\nCantidad inválida o saldo insuficiente. Intente de nuevo.")
-                input("\nPresiona Enter para continuar...")
-                os.system("clear")
-        case 0:
-            os.system("clear")
-            print("\nGracias por usar el cajero automatico")
-            isActive = False
-        case _:
-            print("\nOpción inválida. Intente de nuevo.")
-            input("\nPresiona Enter para continuar...")
-            os.system("clear")
+                
+    except ValueError:
+        print("\Caracter Invalido, Intente de nuevo.")
+        input("\nPresiona Enter para continuar...")
+        os.system("clear")            
             
-        
-        
+            
